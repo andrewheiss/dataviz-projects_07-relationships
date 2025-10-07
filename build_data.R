db190b79d8028ca68a040cf2c14a73b32d1910cd
Andrew Heiss <andrew@andrewheiss.com>
2025-10-07 00:44:41 -0400

    Initial commit

---
+++ build_data.R
@@ -0,0 +1,7 @@
+readr::write_csv(bayesrules::basketball, "data/wnba.csv")
+
+# Build answer key so that the plots to recreate exist in images/
+quarto::quarto_render(
+  "answers.qmd",
+  output_format = c("html", "typst")
+)
